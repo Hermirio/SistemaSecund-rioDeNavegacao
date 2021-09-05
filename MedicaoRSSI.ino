d9a0560b26ae4da4b2d011077d759e918f94f121
Wanderson Hermirio dos Santos <wandersonhermirio@gmail.com>
2021-09-05 14:40:26 -0300

    medição rssi mais com as informações beaons

--- MedicaoRSSI.ino
+++ MedicaoRSSI.ino
@@ -2,26 +2,35 @@
 #include <BLEAdvertisedDevice.h>
 #include <BLEDevice.h>
 #include <BLEScan.h>
+#include <BLEUtils.h>
 
 //variable
 int rssi;
+BLEScan *scan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+    }
+};
 
 void setup() 
 {
   Serial.begin(115200);
-  BLEDevice::init("");
+  BLEDevice::init("Scan Beacon");
 }
 
 void loop()
 {
-  BLEScan *scan = BLEDevice::getScan();
+  scan = BLEDevice::getScan();
   scan->setActiveScan(true);
+  //scan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   BLEScanResults results = scan->start(1);
   for(int i = 0; i < results.getCount(); i++)
   {
     BLEAdvertisedDevice device = results.getDevice(i);
     rssi = device.getRSSI();
-    Serial.print("Sinal: ");
+    Serial.print("Sinal RSSI: ");
     Serial.println(rssi);
   }
   
