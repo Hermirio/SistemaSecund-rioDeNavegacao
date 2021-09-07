609616ce4c5aee5989c85b53c8c3102bbd7df774
wanderson <wandersonhermirio@gmail.com>
2021-09-07 17:17:44 -0300

    Nessa versao esta implementador o metodo para medir RSSI do beacons, e foi removida o objeto MyAdvertisedDeviceCallbacks

--- MedicaoRSSI.ino
+++ MedicaoRSSI.ino
@@ -27,22 +27,19 @@ void loop()
   scan = BLEDevice::getScan();
   scan->setActiveScan(true);
   BLEScanResults results = scan->start(1,false);
-   if(results.getCount() > 0){
-      
-   BLEAdvertisedDevice device = results.getDevice(1);
+  if(results.getCount() > 0)
+  {  
+    BLEAdvertisedDevice device = results.getDevice(1);
 
     mac = device.getAddress().toString().c_str();
     if(mac.equals("ef:42:85:b6:07:e9"))
     {
       rssi = device.getRSSI();
-
       Serial.println(rssi);
       rssi > -80 ? digitalWrite(pino[0], 1): digitalWrite(pino[0], 0);
       rssi > -70 ? digitalWrite(pino[1], 1): digitalWrite(pino[1], 0);
       rssi > -50 ? digitalWrite(pino[2], 1): digitalWrite(pino[2], 0);
       rssi > -40 ? digitalWrite(pino[3], 1): digitalWrite(pino[3], 0);
     }
-   }
-
-  
+  }
 }
