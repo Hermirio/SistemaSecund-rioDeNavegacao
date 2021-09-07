1b71751b3d1faf5255f336edd1ae37190248712a
Wanderson Hermirio dos Santos <wandersonhermirio@gmail.com>
2021-09-07 13:11:35 -0300

    Nessa versão esta pegar RSSI de um dispositivo

--- MedicaoRSSI.ino
+++ MedicaoRSSI.ino
@@ -6,7 +6,9 @@
 
 //variable
 int rssi;
+String mac;
 BLEScan *scan;
+int pino [] = {15, 4, 5, 19};
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
@@ -18,20 +20,35 @@ void setup()
 {
   Serial.begin(115200);
   BLEDevice::init("Scan Beacon");
+  for(int i = 0; i <4; i++)
+  {
+    pinMode(pino[i],OUTPUT);
+    delay(500);
+      
+  }
 }
 
 void loop()
 {
   scan = BLEDevice::getScan();
   scan->setActiveScan(true);
-  //scan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  BLEScanResults results = scan->start(1);
-  for(int i = 0; i < results.getCount(); i++)
-  {
-    BLEAdvertisedDevice device = results.getDevice(i);
-    rssi = device.getRSSI();
-    Serial.print("Sinal RSSI: ");
-    Serial.println(rssi);
-  }
+  BLEScanResults results = scan->start(1,false);
+   if(results.getCount() > 0){
+      
+   BLEAdvertisedDevice device = results.getDevice(1);
+
+    mac = device.getAddress().toString().c_str();
+    if(mac.equals("ef:42:85:b6:07:e9"))
+    {
+      rssi = device.getRSSI();
+
+      Serial.println(rssi);
+      rssi > -80 ? digitalWrite(pino[0], 1): digitalWrite(pino[0], 0);
+      rssi > -70 ? digitalWrite(pino[1], 1): digitalWrite(pino[1], 0);
+      rssi > -50 ? digitalWrite(pino[2], 1): digitalWrite(pino[2], 0);
+      rssi > -40 ? digitalWrite(pino[3], 1): digitalWrite(pino[3], 0);
+    }
+   }
+
   
 }
